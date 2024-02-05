19e387c371987b8987c41ad9a02c2f7d7d6e293f
radio-satellites <114111180+radio-satellites@users.noreply.github.com>
2024-02-05 10:10:47 -0500

    Update load_tables.js

--- lost/load_tables.js
+++ lost/load_tables.js
@@ -1,11 +1,49 @@
-const xhr = new XMLHttpRequest();
-xhr.open("GET", "https://raw.githubusercontent.com/radio-satellites/UTS_LF_DATA/main/lost.txt");
-xhr.send();
-xhr.responseType = "json";
-xhr.onload = () => {
-  if (xhr.readyState == 4 && xhr.status == 200) {
-    console.log(xhr.response);
-  } else {
-    console.log(`Error: ${xhr.status}`);
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+	delete(xmlHttp);
+    return xmlHttp.responseText;
+}
+
+//First, handle the first table
+let lost_text = httpGet("https://raw.githubusercontent.com/radio-satellites/UTS_LF_TRACKER_DATA/main/lost.txt").split(/\r?\n/);
+//console.log(lost_text);
+
+lost_text.splice(-1);
+lost_text.shift();
+
+var table = document.getElementById("lostTable");
+
+for (let i = 0; i < lost_text.length; i++) {
+  var row = table.insertRow(1);
+  let line = lost_text[i];
+  let cells = line.split(",");
+  for (let x = 0; x < cells.length; x++){
+	var cell = row.insertCell(x);
+	cell.innerHTML = cells[x];
   }
-};
+  //console.log(cells);
+}
+
+//Now the other
+
+let found_text = httpGet("https://raw.githubusercontent.com/radio-satellites/UTS_LF_TRACKER_DATA/main/found.txt").split(/\r?\n/);
+//console.log(lost_text);
+
+found_text.splice(-1);
+found_text.shift();
+
+table = document.getElementById("recentlyFoundTable");
+
+for (let i = 0; i < lost_text.length; i++) {
+  row = table.insertRow(1);
+  line = found_text[i];
+  cells = line.split(",");
+  for (let x = 0; x < cells.length; x++){
+	var cell = row.insertCell(x);
+	cell.innerHTML = cells[x];
+  }
+  //console.log(cells);
+}
